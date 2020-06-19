54f10d0a6f752ff6944522647107807794295346
zbeok <scao95014@gmail.com>
2020-06-19 11:37:25 -0700

    deleted unnecessary files

--- public/js/controllers/CareersCtrl.js
+++
@@ -1,5 +0,0 @@
-angular.module('CareersCtrl', []).controller('CareersController', function($scope,$http,$routeParams) {
-
-	
-
-});
--- public/js/controllers/ProductsCtrl.js
+++
@@ -1,4 +0,0 @@
-angular.module('ProductsCtrl', []).controller('ProductsController', function($scope,$http) {
-	
-
-});
